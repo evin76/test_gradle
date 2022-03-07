4b5a584ca6a6daf3d8131a7238ac12ea5d97dac8
evin76 <gengerw1604@gmail.com>
2022-03-07 14:35:04 +0500

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.joda.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args){
+        LocalTime time = new LocalTime();
+        System.out.println(time);
+        System.out.println("Hello World");
+    }
+}
